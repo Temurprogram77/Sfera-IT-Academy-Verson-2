41d182bd267316eba7236cccb2a6bdbe122eabca
BekjonUz <mightyking584@gmail.com>
2026-01-06 17:11:56 +0500

    add

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -72,17 +72,17 @@ export default function UserDropdown() {
         {/* Dropdown Items */}
         <ul className="flex flex-col gap-1 pt-4 pb-3 border-b border-gray-200 dark:border-gray-800">
           <li>
-            <DropdownItem onItemClick={closeDropdown} tag="a" to="/profile">
+            <DropdownItem onItemClick={closeDropdown} className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300" tag="a" to="/profile">
               {t("edit_profile")}
             </DropdownItem>
           </li>
           <li>
-            <DropdownItem onItemClick={closeDropdown} tag="a" to="/profile">
+            <DropdownItem className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300" onItemClick={closeDropdown} tag="a" to="/profile">
               {t("account_settings")}
             </DropdownItem>
           </li>
           <li>
-            <DropdownItem onItemClick={closeDropdown} tag="a" to="/profile">
+            <DropdownItem className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300" onItemClick={closeDropdown} tag="a" to="/profile">
               {t("support")}
             </DropdownItem>
           </li>
